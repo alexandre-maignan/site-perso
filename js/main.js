383ee53bfad528f8ee88f7af08b55c9fe01a7965
Alexandre <alex.maignan95@gmail.com>
2026-09-25 18:40:18 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,3 +7,35 @@ const currentYear = document.getElementById("current-year");
 if (currentYear) {
     currentYear.textContent = new Date().getFullYear();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.querySelectorAll("a").forEach(link => {
+    link.addEventListener("click", e => {
+        const targetURL = new URL(link.href, window.location.href);
+
+        if (
+            targetURL.pathname === window.location.pathname &&
+            targetURL.search === window.location.search &&
+            targetURL.hash === window.location.hash
+        ) {
+            e.preventDefault();
+        }
+    });
+});
